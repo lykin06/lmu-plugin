3b38a6d280f768bf2d8068b1afddb4c7a48af5b6
Mathieu Boutelier <lykin06@gmail.com>
2016-01-16 17:07:06 +0100

    Added plugin, modified to be visible when .jar files are selected

---
+++ src/fr/unice/polytech/lmu_plugin/SMtoNuSMV.java
@@ -0,0 +1,32 @@
+package fr.unice.polytech.lmu_plugin;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class SMtoNuSMV implements IHandler {
+
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	public void dispose() {
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public boolean isHandled() {
+		return true;
+	}
+
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+
+}
