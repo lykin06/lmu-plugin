2d79515108a6f53c6fdaf637a31a5211ae0d6523
Mathieu Boutelier <lykin06@gmail.com>
2016-02-19 12:22:53 +0100

    added dependency analisys functions in Connector for the plugin

--- src/fr/unice/polytech/lmu/Connector.java
+++ src/fr/unice/polytech/lmu/Connector.java
@@ -15,43 +15,55 @@ import analysis.Analyzer;
 public class Connector {
 	Model model;
 	static String extension;
-	
+
 	public Connector() {
 		model = new Model();
 	}
-	
+
 	private static File checkInput(String input) throws IOException {
 		extension = FileChooser.getFileExtension(input);
 		if (extension.equals("jar")) {
 			return new File(input);
 		}
-		
+
 		throw new IOException("Bad input file extension");
 	}
 	
-	public void exportJar(String jarLocation, String exportDir, String exportFormat)
-			throws ParseError, IOException, WriterException {
-		checkInput(jarLocation);
-		File output = new File(exportDir + "." + exportFormat);
-		
-		Analyzer analyzer = (Analyzer) Analyzer.getModelFactory("analyzer");
-		
-		switch(extension){
-		case "java" :
-			break;
-		case "jar" :
-				model = analyzer.jarAnalysis(jarLocation);
-				break;
-		default :
-			break;					
-	}
-		
+	private static void output(String outputFileName, Model model) throws IOException, WriterException {
+		File output = new File(outputFileName);
 		AbstractWriter factory = AbstractWriter.getTextFactory(FileChooser.getFileExtension(output.getName()));
-		
-		
 		FileOutputStream fis = new FileOutputStream(output);
 		fis.write(factory.writeModel(model));
 		fis.flush();
 		fis.close();
+	}
+
+	public void exportJar(String jarLocation, String exportDir, String exportFormat)
+			throws ParseError, IOException, WriterException {
+		checkInput(jarLocation);
+		String outputFileName = exportDir + "." + exportFormat;
+
+		Analyzer analyzer = (Analyzer) Analyzer.getModelFactory("analyzer");
+
+		switch (extension) {
+		case "java":
+			break;
+		case "jar":
+			model = analyzer.jarAnalysis(jarLocation);
+			break;
+		default:
+			break;
+		}
+		
+		output(outputFileName, model);
+	}
+	
+	public void jarDenpendencies(String jarLocation, String exportDir, String exportFormat)
+			throws IOException, WriterException {
+		checkInput(jarLocation);
+		String outputFileName = exportDir + "." + exportFormat;
+		Analyzer analyzer = (Analyzer) Analyzer.getModelFactory("analyzer");
+		model = analyzer.dependencyAnalysis(jarLocation);
+		output(outputFileName, model);
 	}
 }
